5d432ff1555a7c4a2374a18c22369ad95366df5d
Michael Baudino <mike@kamehouse.biz>
2014-02-09 22:29:45 +0100

    Stop using a plugin to display posts on homepage but use our own shortcode

--- wp-content/themes/tyrolienne/lib/custom.php
+++ wp-content/themes/tyrolienne/lib/custom.php
@@ -2,3 +2,21 @@
 /**
  * Custom functions
  */
+
+//[foobar]
+function tyrolienne_news_func($atts) {
+  extract(shortcode_atts(array('count' => 5), $atts));
+
+  query_posts(array('post_type' => 'post', 'posts_per_page' => $count));
+
+  ob_start();
+
+  ?>
+    <?php while (have_posts()) : the_post(); ?>
+      <?php get_template_part('templates/content', get_post_format()); ?>
+    <?php endwhile; ?>
+  <?php
+
+  return ob_get_clean();
+}
+add_shortcode('tyrolienne_news', 'tyrolienne_news_func' );
