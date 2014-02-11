2c43b501db08bdc8303740091a4f6fe75bd2c681
Michael Baudino <mike@kamehouse.biz>
2014-02-11 21:28:08 +0100

    Add target="_blank" to header links

--- wp-content/themes/tyrolienne/templates/header-top-navbar.php
+++ wp-content/themes/tyrolienne/templates/header-top-navbar.php
@@ -20,41 +20,41 @@
       <div class="resorts-content">
 
         <div class="resort valthorens">
-          <a href="http://www.valthorens.com">
+          <a href="http://www.valthorens.com" target="_blank">
             <img class="logo" src="<?= image_asset('logo-valthorens-small.png') ?>"></img>
           </a>
           <div class="social-networks">
-            <a href="https://twitter.com/Val_Tho">
+            <a href="https://twitter.com/Val_Tho" target="_blank">
               <img src="<?= image_asset('icon-twitter.png') ?>">
             </a>
-            <a href="https://www.youtube.com/user/valthoweb">
+            <a href="https://www.youtube.com/user/valthoweb" target="_blank">
               <img src="<?= image_asset('icon-youtube.png') ?>">
             </a>
-            <a href="https://www.facebook.com/valthorens.resort">
+            <a href="https://www.facebook.com/valthorens.resort" target="_blank">
               <img src="<?= image_asset('icon-facebook.png') ?>">
             </a>
           </div>
         </div>
 
         <div class="resort orelle">
-          <a href="http://www.orelle.net">
+          <a href="http://www.orelle.net" target="_blank">
             <img class="logo" src="<?= image_asset('logo-orelle-small.png') ?>"></img>
           </a>
           <div class="social-networks">
-            <a href="https://www.youtube.com/user/OTOrelle">
+            <a href="https://www.youtube.com/user/OTOrelle" target="_blank">
               <img src="<?= image_asset('icon-youtube.png') ?>">
             </a>
-            <a href="http://instagram.com/orelle_les3vallees">
+            <a href="http://instagram.com/orelle_les3vallees" target="_blank">
               <img src="<?= image_asset('icon-instagram.png') ?>">
             </a>
-            <a href="https://www.facebook.com/pages/Orelle-Les-3-Vall%C3%A9es/116306588613">
+            <a href="https://www.facebook.com/pages/Orelle-Les-3-Vall%C3%A9es/116306588613" target="_blank">
               <img src="<?= image_asset('icon-facebook.png') ?>">
             </a>
           </div>
         </div>
 
         <div class="s3v">
-          <a href="http://www.les3vallees.com">
+          <a href="http://www.les3vallees.com" target="_blank">
             <img src="<?= image_asset('3vallees.png') ?>">
           </a>
         </div>
