c4e215700e0edb1dc5de4b78b88ba429854be6c7
Michael Baudino <mike@kamehouse.biz>
2014-02-11 22:08:51 +0100

    Add favicon

--- wp-content/themes/tyrolienne/templates/head.php
+++ wp-content/themes/tyrolienne/templates/head.php
@@ -5,6 +5,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="apple-touch-icon" href="<?= image_asset('favicon.png') ?>">
+  <link rel="shortcut icon" href="<?= image_asset('favicon.png') ?>">
 
   <?php wp_head(); ?>
 
