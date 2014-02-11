f8b2e1da171be3e21962557b22ebdee61db3029c
Michael Baudino <mike@kamehouse.biz>
2014-02-11 21:29:11 +0100

    Change featured image height

--- wp-content/themes/tyrolienne/lib/scripts.php
+++ wp-content/themes/tyrolienne/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f957c34265708a6440f6c43ca5cb4313');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e9c32305477459df87ae190e0e0fa349');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
