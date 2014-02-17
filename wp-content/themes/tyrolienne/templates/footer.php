05ca6877d8a690c0514538ff8bbf5732f799b269
Michael Baudino <mike@kamehouse.biz>
2014-02-17 17:18:23 +0100

    Minor style changes (in the footer)

--- wp-content/themes/tyrolienne/templates/footer.php
+++ wp-content/themes/tyrolienne/templates/footer.php
@@ -1,18 +1,20 @@
 <footer class="content-info" role="contentinfo">
 
-  <div class="logos">
-    <a href="http://www.orelle.net" target="_blank">
-      <img class="logo" src="<?= image_asset('logo-orelle-small.png') ?>"></img>
-    </a>
-    <a href="http://www.valthorens.com" target="_blank">
-      <img class="logo" src="<?= image_asset('logo-valthorens-small.png') ?>"></img>
-    </a>
-    <a href="http://www.les3vallees.com" target="_blank">
-      <img src="<?= image_asset('3vallees.png') ?>">
-    </a>
-  </div>
+  <div class="container">
+    <div class="logos">
+      <a href="http://www.orelle.net" target="_blank">
+        <img class="logo" src="<?= image_asset('logo-orelle-small.png') ?>"></img>
+      </a>
+      <a href="http://www.valthorens.com" target="_blank">
+        <img class="logo" src="<?= image_asset('logo-valthorens-small.png') ?>"></img>
+      </a>
+      <a href="http://www.les3vallees.com" target="_blank">
+        <img src="<?= image_asset('3vallees.png') ?>">
+      </a>
+    </div>
 
-  <?php wp_nav_menu() ?>
+    <?php wp_nav_menu() ?>
+  </div>
 
 </footer>
 
