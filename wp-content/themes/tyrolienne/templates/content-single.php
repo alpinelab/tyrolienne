6d507b4d526fba5f545adb42ce9eca4716c90c38
Michael Baudino <mike@kamehouse.biz>
2014-02-17 17:56:46 +0100

    Fix a display bug on single posts pages with aside formats

--- wp-content/themes/tyrolienne/templates/content-single.php
+++ wp-content/themes/tyrolienne/templates/content-single.php
@@ -1,19 +1,20 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
+
     <header>
-      <h2 class="entry-title"><?php the_title(); ?></h1>
+      <h3 class="entry-title"><?php the_title(); ?></h3>
       <?php get_template_part('templates/entry-meta'); ?>
     </header>
-    <? if (has_post_thumbnail()) { ?>
-      <? $featured_image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'large'); ?>
-      <div class="post-thumbnail" style="background-image: url('<?= $featured_image[0] ?>');"></div>
-    <? } ?>
+
+    <? if (has_post_thumbnail()) { // Featured image or featured video
+      echo the_post_thumbnail('large');
+    } else { // Carousel
+      echo do_shortcode('[carousel]');
+    } ?>
+
     <div class="entry-content">
       <?php the_content(); ?>
     </div>
-    <footer>
-      <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
-    </footer>
-    <?php comments_template('/templates/comments.php'); ?>
+
   </article>
 <?php endwhile; ?>
