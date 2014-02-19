07571c9d08f7f1ee18677c36fb7ab557f3f90ffa
Michael Baudino <mike@kamehouse.biz>
2014-02-19 14:43:35 +0100

    Fix a bug with minified header

--- wp-content/themes/tyrolienne/lib/scripts.php
+++ wp-content/themes/tyrolienne/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '43c419d64e71ca54de5401f9526a781c');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '38460c2b1b79d7cbdab493845033507f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
