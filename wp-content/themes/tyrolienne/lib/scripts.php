285fbba5a24d518a81218af9a535357591bb160c
Michael Baudino <mike@kamehouse.biz>
2014-02-19 17:02:09 +0100

    Display English content in italic

--- wp-content/themes/tyrolienne/lib/scripts.php
+++ wp-content/themes/tyrolienne/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '38460c2b1b79d7cbdab493845033507f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '71bbd91da4fc755b17c81faaacf7bc77');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
