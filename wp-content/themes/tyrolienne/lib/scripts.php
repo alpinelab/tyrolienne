c7d9b131dad38c95d319a24560b072ff02051f71
Michael Baudino <mike@kamehouse.biz>
2014-03-05 10:40:37 +0100

    Remove double horizontal line on pages

--- wp-content/themes/tyrolienne/lib/scripts.php
+++ wp-content/themes/tyrolienne/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '71bbd91da4fc755b17c81faaacf7bc77');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5ebca0817268048def0aa20788308f31');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
