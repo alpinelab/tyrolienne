7d39d38acc3071d5c47e4e9469aaf58443253dc8
Michael Baudino <mike@kamehouse.biz>
2014-03-11 10:44:37 +0100

    Add space between the date and the post title

--- wp-content/themes/tyrolienne/lib/scripts.php
+++ wp-content/themes/tyrolienne/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '934d03a7b8b085e69c4e9dc3f0e8a277');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '89295a3fe6fa42e52e5641028cb67d32');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
